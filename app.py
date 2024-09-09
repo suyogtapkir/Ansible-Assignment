5e2c753eb40ce059b353a72b3d7073afe96495cc
root <root@ip-172-31-33-19.eu-north-1.compute.internal>
2024-09-09 13:07:24 +0000

    commit from aws-ec2 instance

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
